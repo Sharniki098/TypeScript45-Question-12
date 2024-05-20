1e8737ad0cbbe30084baa36d1f788423a54be0f9
Shumaila Noreen <161513956+Sharniki098@users.noreply.github.com>
2024-05-20 01:27:10 -0700

    Add files via upload

---
+++ Greetings.js
@@ -0,0 +1,5 @@
+var names = ["Muskan", "Eman", "Aria", "Mel"];
+var message = "Welcome to typescript class:";
+for (var i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ Greetings.ts
@@ -0,0 +1,7 @@
+let names :string[]=["Muskan","Eman","Aria","Mel"];
+
+const message:string ="Welcome to typescript class:"
+
+for(let i=0; i<names.length; i++){
+    console.log(message+names[i]);
+}
